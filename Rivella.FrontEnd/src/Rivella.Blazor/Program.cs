9847c252c129db4a74c3ddfe52393c8ac98339b4
gabriel <gabriel.maurici@hotmail.com>
2024-12-29 20:54:13 -0300

    add appsettings configuração base url rivella api

--- Rivella.FrontEnd/src/Rivella.Blazor/Program.cs
+++ Rivella.FrontEnd/src/Rivella.Blazor/Program.cs
@@ -7,13 +7,22 @@ using Rivella.Blazor.Services.Album.UploadPhoto;
 using Rivella.Blazor.Services.RivellaApi;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var environment = builder.HostEnvironment.IsDevelopment() ? "Development" : "Production";
+builder.Configuration.AddJsonFile(
+    $"appsettings.{environment}.json",
+    optional: false,
+    reloadOnChange: true
+);
+
 builder.Services.AddHttpClient<IRivellaApiService, RivellaApiService>("rivella-api", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7075/");
+    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("rivella-api")!);
 });
+
 builder.Services.AddTransient<IRivellaApiService, RivellaApiService>();
 builder.Services.AddTransient<ICreateAlbumService, CreateAlbumService>();
 builder.Services.AddTransient<IUploadPhotoService, UploadPhotoService>();
